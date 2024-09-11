063de674779ee1c14fc8510287641dc143a00753
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-11 12:04:41 +0300

    add footer layout

--- src/components/FooterLayout.vue
+++ src/components/FooterLayout.vue
@@ -5,12 +5,81 @@ function getCurrentYear() {
 </script>
 
 <template>
-    <footer class="footer">
-        <div class="container"></div>
+    <footer class="footer" style="">
+        <div class="footer__wrapper">
+            <div class="container">
+                <div class="footer__mail">
+                    <a href="mailto:info@australianbestcasinohotels.com">info@australianbestcasinohotels.com</a>
+                </div>
+
+                <div class="footer__nav">
+                    <ul>
+                        <li>
+                            <router-link to="/privacy" class="header__logo">
+                                <span>Privacy Policy</span>
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/terms" class="header__logo">
+                                <span>Terms & Conditions</span>
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/cookies" class="header__logo">
+                                <span>Cookies Policy</span>
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+        <div class="footer__copyright">
+            <div class="container">
+                <span>Australianbestcasinohotels.com - All Rights Reserved © {{ getCurrentYear() }}</span>
+            </div>
+        </div>
     </footer>
 </template>
 
 <style scoped lang="sass">
 @import '../assets/styles/main'
+
+.footer
+    color: #b0b0b0
+
+    &__wrapper
+        padding: 80px 0 80px 0
+        background: url('/footer-banner.jpg') bottom center / cover no-repeat
+        font-size: 14px
+        line-height: 22px
+
+        .container
+            display: flex
+            justify-content: center
+            gap: 40px
+
+    &__nav
+        ul
+            display: flex
+            flex-direction: column
+            gap: 10px
+
+
+    &__copyright
+        background: #000
+        padding: 25px 0
+        font-size: 14px
+        color: #b0b0b0
+
+
+@media (max-width: 768px)
+    .footer
+        &__wrapper
+            .container
+                flex-direction: column
+        
+        &__copyright
+            text-align: center
 
 </style>
