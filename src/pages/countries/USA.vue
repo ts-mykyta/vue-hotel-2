201cfeb59e833d2a69edbb5fff74535f5a085956
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-23 11:19:50 +0300

    change 4 hotels on USA page and Home page

--- src/pages/countries/USA.vue
+++ src/pages/countries/USA.vue
@@ -8,25 +8,25 @@ import ScrollToTop from '../../components/ScrollToTop.vue';
 
 const hotelsData = [
     {
-        "imgUrl": "/countries/usa/refuge-inn.jpg",
-        "hotelName": "Refuge Inn",
+        "imgUrl": "/countries/usa/atlantis-casino.jpg",
+        "hotelName": "Atlantis Casino Resort Spa",
         "starsCount": 4,
-        "hotelLink": "https://www.refugeinn.com/",
-        "hotelDescr": "Exceed your expectations at this family owned inn where no detail is overlooked. The Refuge Inn is located within walking distance of the wildlife refuge and minutes from our beautiful undeveloped beach. Amenities include Continental Breakfast, Wireless Internet, Indoor/Outdoor Heated Pool, Hot Tub, Sauna, Fitness Room, Observation Decks, Bicycle & Beach Gear Rentals, Conference Room and Gift Shop. Rooms are continually updated and renovated. At the Refuge Inn, you arrive as guests and leave as friends, and are treated as part of the family. A Treasure of a place - Local Family Owned and Proudly Operated."
+        "hotelLink": "https://atlantiscasino.com/",
+        "hotelDescr": "Nestled at the base of the Sierra Nevada, Atlantis Casino Resort Spa is proud to be recognized as a AAA Four Diamond resort - a distinction held by only the most exceptional hotels in the world. Our luxury hotel rooms and suites feature floor-to-ceiling windows overlooking incredible views of the city or the Sierra Nevada Mountains. Whether you're looking for a fun casino getaway or planning a trip of a lifetime, Atlantis offers something to satisfy every guest."
     },
     {
-        "imgUrl": "/countries/usa/hotel-lucia.jpg",
-        "hotelName": "Hotel Lucia",
+        "imgUrl": "/countries/usa/silverton-casino-hotel.jpg",
+        "hotelName": "Silverton Casino Lodge",
         "starsCount": 4,
-        "hotelLink": "https://www.hotellucia.com/",
-        "hotelDescr": "Want to get to know the real Portland? The keeping weird, coffee-drinking, food-loving, creative, always-outdoors, never normal Portland? We're Hotel Lucia, and we know Portland. Because we are Portland. Centrally located in the heart of downtown close to dining, shopping, entertainment, public transit, and more, Hotel Lucia is the perfect starting point for your Portland adventures. Why? From our hotel artwork to the experts at our 24-hr front desk and concierge service, everything about us is local. We’ve been helping people discover and fall in love with this city since before Portland was trendy, and we’ll keep on doing it for everyone from leisure travelers to business travelers and everyone in between."
+        "hotelLink": "https://silvertoncasino.com",
+        "hotelDescr": "Silverton Casino's newly renovated lodge features a boutique hotel experience. Step into our elegantly redesigned rooms, with a \"rustic luxe\" ambiance and a contemporary twist on the classic lodge theme. Located less than 5 miles from the Las Vegas Strip, Silverton features its own 90,000-square-foot casino and 117,000-gallon aquarium. Ten award-winning bars and restaurants include Twin Creeks Steakhouse, named the best casino restaurant by readers of Las Vegas Review-Journal. The lodge also includes a flagship Bass Pro Shops."
     },
     {
-        "imgUrl": "/countries/usa/the-whittaker-inn.jpg",
-        "hotelName": "The Whittaker Inn",
+        "imgUrl": "/countries/usa/thunder-valley.jpg",
+        "hotelName": "Thunder Valley Casino Resort",
         "starsCount": 4,
-        "hotelLink": "https://www.thewhittakerinn.com/",
-        "hotelDescr": "Built from the ground up on 25 acres of secluded woodland, this Inn offers a refreshing escape from the ordinary. The Inn is located only five miles from Purdue University campus and 2.5 miles from the award-winning golf course at Coyote Crossing. Inspired by the Whittakers' passion for travel, each of The Whittaker's accommodations are custom-designed and equipped with modern amenities."
+        "hotelLink": "https://thundervalleyresort.com/",
+        "hotelDescr": "Thunder Valley is a AAA Four Diamond award-winning Casino Resort, neighboring the Roseville/Rocklin Area, Roseville Amtrak station, @theGrounds Events Venue, Topgolf Roseville, and Roseville Galleria Mall. The casino features a 250,000-square-foot Las Vegas-style casino, offering over 3,400 slot and video machines, 100 table games and a live Poker Room which accommodates more than 250 players. Thunder Valley includes a 17-story luxury hotel with 408 rooms, including 46 suites, a full amenity Spa, and Gift Shop. The resort features 14 restaurants and bars, as well as Pano Hall, a large banquet and entertainment center with a capacity of up to 900 guests."
     },
     {
         "imgUrl": "/countries/usa/captain-david-kelley.jpg",
@@ -36,11 +36,11 @@ const hotelsData = [
         "hotelDescr": "A warm welcome awaits you. The Captain David Kelley House is an upscale bed & breakfast set in a restored Greek Revival sea captain's home built in 1835 in the heart of Cape Cod. Architectural gem featuring hardwood floors, crown moldings, and wainscoting. Three covered porches overlook beautifully landscaped yard and gardens. Listed on National Register of Historic Places, the inn offers six beautiful guest rooms with private baths, A/C, flat panel TVs, WI-FI and hair dryers. Beautiful beaches are just a 1/2 mile walk down the street, Hyannis, Osterville and Barnstable are a 10 minute ride away. Breakfast is truly an event each morning, including our famous \"Scone Wars\", featured on TV!"
     },
     {
-        "imgUrl": "/countries/usa/port-d-hiver.jpg",
-        "hotelName": "Port D'hiver Bed And Breakfast",
+        "imgUrl": "/countries/usa/live-casino-hotel-philadelphia.jpg",
+        "hotelName": "Live Casino & Hotel Philadelphia",
         "starsCount": 4,
-        "hotelLink": "https://www.portdhiver.com/",
-        "hotelDescr": "Escape your everyday cares at our romantic boutique hotel in Melbourne Beach, a Select Registry Property. Our comfortable yet elegant Melbourne Beach inn is only 200 feet from the Atlantic Ocean and features luxurious rooms and suites with gorgeous ocean and pool views. Indulge in our array of amenities, including an onsite fitness center, an outdoor fire pit, and a serene spa pool surrounded by lush tropical greenery. Take advantage of our cruiser bikes and complimentary beach supplies to explore the nearby parks, restaurants, and beaches at your leisure."
+        "hotelLink": "https://philadelphia.livecasinohotel.com/hotel",
+        "hotelDescr": "Live! Casino & Hotel Philadelphia is an unrivaled destination to enjoy world-class gaming, luxury hotel accommodations, memorable entertainment, and premier dining. Live! features more than 200 luxury hotel rooms awarded Tripadvisor Traveler’s Choice “Best of the Best” and are Sharecare Health Security VERIFIED™ with Forbes Travel Guide. Conveniently located only a short walk from Philadelphia’s stadiums & Xfinity Live! – as well in proximity to many Philadelphia historical monuments and attractions. Live! Casino & Hotel Philadelphia also features over 2,000 slot machines and electronic table games, FanDuel® Sportsbook, and 140 live action table games, including a dedicated poker room."
     },
     {
         "imgUrl": "/countries/usa/the-belmont-1857.jpg",
